76f764a28b35c85fd836430c848e917e656cb05e
asnganje <abd.nganje@gmail.com>
2024-06-03 06:58:33 +0300

    Add data elements

--- src/data.jsx
+++ src/data.jsx
@@ -3,3 +3,55 @@ import { RiBarChartFill } from "react-icons/ri";
 import { MdEmail, MdOutlineStickyNote2 } from "react-icons/md";
 import { IoStarSharp } from "react-icons/io5";
 import { GoProjectSymlink } from "react-icons/go";
+import { nanoid } from "nanoid";
+
+export const data = [
+    {
+        id: nanoid(),
+        url: '/about',
+        text:'About',
+        icon: <FaUser />
+    },
+    {
+        id: nanoid(),
+        url: '/experience',
+        text:'Experience',
+        icon: <FaArrowTrendUp />
+    },
+    {
+        id: nanoid(),
+        url: '/projects',
+        text:'Projects',
+        icon: <GoProjectSymlink />
+    },
+    {
+        id: nanoid(),
+        url: '/skills',
+        text:'Skills',
+        icon: <RiBarChartFill />
+    },
+    {
+        id: nanoid(),
+        url: '/awards',
+        text:'Awards',
+        icon: <IoStarSharp />
+    },
+    {
+        id: nanoid(),
+        url: '/education',
+        text:'Education',
+        icon: <FaGraduationCap />
+    },
+    {
+        id: nanoid(),
+        url: '/contact',
+        text:'Contact',
+        icon: <MdEmail />
+    },
+    {
+        id: nanoid(),
+        url: '/resume',
+        text:'Resume',
+        icon: <MdOutlineStickyNote2 />
+    }
+]
