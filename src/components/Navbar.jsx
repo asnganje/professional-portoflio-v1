6277c9b23704c89ba01bd1fe19a006c683046a1b
asnganje <abd.nganje@gmail.com>
2024-06-03 09:47:41 +0300

    Add Hamburg and Logo to Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import Logo from "./Logo";
+import Hamburg from "./Hamburg";
+
+const Navbar = () => {
+    return <nav className="main-nav">
+                <Hamburg />
+                <Logo />
+        </nav>
+}
+
+export default Navbar;
