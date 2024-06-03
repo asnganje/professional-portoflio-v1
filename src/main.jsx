454a2699f6b0e6742b4bdf33f679b02bc5677e3a
asnganje <abd.nganje@gmail.com>
2024-06-03 16:34:11 +0300

    Add provider to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AppProvider } from './context.jsx'
 
 const el = document.getElementById('root')
 const root = createRoot(el)
 root.render(
-    <App />
+    <AppProvider>
+        <App />
+    </AppProvider>
 )
