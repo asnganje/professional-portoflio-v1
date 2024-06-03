4f9488f1a78688305b3c356458016e2ccf73c9cc
asnganje <abd.nganje@gmail.com>
2024-06-03 16:35:42 +0300

    pass context to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,8 @@
+import { useGlobalContext } from "../context";
+
 const About = () => {
+    const {isSidebarOpen} = useGlobalContext()
+    console.log(isSidebarOpen);
     return <h2>About</h2>
 }
 
