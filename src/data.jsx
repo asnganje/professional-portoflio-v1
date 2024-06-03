6bac430a1faed11ece4bd6538edd2d721d1845a3
asnganje <abd.nganje@gmail.com>
2024-06-03 06:49:30 +0300

    Add icons to data jsx

---
+++ src/data.jsx
@@ -0,0 +1,5 @@
+import {FaArrowTrendUp, FaUser, FaGraduationCap } from "react-icons/fa6";
+import { RiBarChartFill } from "react-icons/ri";
+import { MdEmail, MdOutlineStickyNote2 } from "react-icons/md";
+import { IoStarSharp } from "react-icons/io5";
+import { GoProjectSymlink } from "react-icons/go";
