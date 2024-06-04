de6867da27df8ac8ee014cdad7689c309d22a5de
asnganje <abd.nganje@gmail.com>
2024-06-04 15:22:53 +0300

    Modify data to include icon color

--- src/data.jsx
+++ src/data.jsx
@@ -10,48 +10,56 @@ export const data = [
         id: nanoid(),
         url: '/about',
         text:'About',
-        icon: <FaUser />
+        icon: <FaUser />,
+        color:'#008073'
     },
     {
         id: nanoid(),
         url: '/experience',
         text:'Experience',
-        icon: <FaArrowTrendUp />
+        icon: <FaArrowTrendUp />,
+        color:'#00bcd4'
     },
     {
         id: nanoid(),
         url: '/projects',
         text:'Projects',
-        icon: <GoProjectSymlink />
+        icon: <GoProjectSymlink />,
+        color:'#3f51b5'
     },
     {
         id: nanoid(),
         url: '/skills',
         text:'Skills',
-        icon: <RiBarChartFill />
+        icon: <RiBarChartFill />,
+        color:'#9c27b0'
     },
     {
         id: nanoid(),
         url: '/awards',
         text:'Awards',
-        icon: <IoStarSharp />
+        icon: <IoStarSharp />,
+        color:'#F44336'
     },
     {
         id: nanoid(),
         url: '/education',
         text:'Education',
-        icon: <FaGraduationCap />
+        icon: <FaGraduationCap />,
+        color:'#ff9800'
     },
     {
         id: nanoid(),
         url: '/contact',
         text:'Contact',
-        icon: <MdEmail />
+        icon: <MdEmail />,
+        color:'#795548'
     },
     {
         id: nanoid(),
         url: '/resume',
         text:'Resume',
-        icon: <MdOutlineStickyNote2 />
+        icon: <MdOutlineStickyNote2 />,
+        color:'#424242'
     }
 ]
