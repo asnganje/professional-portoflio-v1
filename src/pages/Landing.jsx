da31c13ba38bc11574880db5b0a763c358a46d0c
asnganje <abd.nganje@gmail.com>
2024-06-05 15:08:39 +0300

    Modify landing page outlook

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,11 @@
+import profImg from '../assets/profileImg.jpeg'
+
 const Landing = () => {
-    return <h2 className="land">Landing site</h2>
+    return <div className="land">
+                <h3 className='top-desc'>I want to <span className='things'>create things</span> <br/> that truly  
+                <span> </span><span className='diff'>make a difference</span></h3>
+                <img src={profImg} alt="profile-pic" className='profile-pic'/>
+        </div>
 }
 
 export default Landing;
