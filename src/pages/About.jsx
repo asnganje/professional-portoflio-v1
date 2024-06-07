a6dd074918f8623a0f11bdd44ad630bf24eacbf6
asnganje <abd.nganje@gmail.com>
2024-06-07 22:37:14 +0300

    Add about component content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,34 @@ import { useGlobalContext } from "../context";
 const About = () => {
     const {isSidebarOpen} = useGlobalContext()
     return <div className="abt">
-            <Header title="About Us"/>
+            <Header title="About"/>
+            <div className="about-div">
+                <blockquote>
+                <h2>
+                    Goal-oriented full stack developer 
+                    dedicated to meticulous UI design, 
+                    teamwork and teaching.
+                </h2>
+                </blockquote>
+                <p>
+                My career has been diverse, spanning roles as a 
+                fisherman, a local authority employee of 
+                the year, frontend developer in a tour 
+                firm website, mentor at an online coding school, 
+                FullStack developer, and a small business owner. Throughout these experiences, 
+                I've remained committed to helping others and solving problems with passion."
+                </p>
+                <p>
+
+                    As a web developer, I leverage my meticulous attention to detail, unwavering passion for creation, and purpose-driven work ethic to make a tangible difference. This is why I'm eager to contribute to a rapidly growing company and drive significant impact.
+                </p>
+                <h3>
+                    <strong>Current Focus:</strong>
+                    <a href="https://www.mongodb.com/resources/languages/mern-stack-tutorial" target="_blank">
+                    The MERN Stack
+                    </a>
+                </h3>
+            </div>
         </div>
 }
 
