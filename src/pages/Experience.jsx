f6dc9d5f4348adf7f012e55a1a8b70caa7c4f571
asnganje <abd.nganje@gmail.com>
2024-06-11 14:39:33 +0300

    Add experience content

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -6,12 +6,52 @@ import { data } from "./experience-data";
 const Experience = () => {
     return <div className="exp">
                 <Header title="Experience"/>
-                <div>
+                <div className="expe-div">
                   {
                     data.map((item)=> {
+                      const {
+                              id, 
+                              logo,
+                              companyUrl, 
+                              companyName,
+                              role,
+                              companyDesc,
+                              period,
+                              location,
+                              responsibilities
+                            } = item
                       return(
-                        <div>
-                          
+                        <div key={id} className="exp-item">
+                          <div className="exp-item-header">
+                            <span className={companyName === 'Pemba Beach Resort' && 'addBg'}>
+                              <img src={logo} alt={companyName} />
+                            </span>
+                            <a href={companyUrl} target="_blank">
+                              {companyName}
+                            </a>
+                            <p>
+                              {role}
+                            </p>
+                          </div>
+                          <hr />
+                          <div className="exp-div-content">
+                            <p>{companyDesc}</p>
+                            <ul className="exp-ul">
+                              {
+                                responsibilities.map((item)=> {
+                                  const {id,task} = item
+                                  return(
+                                    <li key={id}>
+                                      {task}
+                                    </li>
+                                  )
+                                })
+                              }
+                            </ul>
+                            <p className="exp-loc-period">
+                              <span>{period}</span> | {location}
+                            </p>
+                          </div>
                         </div>
                       )
                     })
--- src/pages/experience-data.js
+++ src/pages/experience-data.js
@@ -9,54 +9,75 @@ import cgk from '../assets/cgk.png';
 export const data = [
   {
     id: nanoid(),
-    logo: tecafili,
-    companyUrl: 'https://tecafili.org/',
-    companyName: 'TECAFILI',
-    role: 'IT Support Technician',
-    companyDesc: 'Technology for care and Livelihood - Non governmental Organization focusing on  training of fishers in mariculture',
-    period: 'April - Dec, 2014',
-    location: 'Mombasa, KE',
+    logo: pemba,
+    companyUrl:'https://pembaparadise.com/',
+    companyName: 'Pemba Beach Resort',
+    role: 'Web Developer',
+    companyDesc: 'Tour firm specializing in providing hotel services in Zanzibar',
+    period: 'Jan, 2024 - To Date',
+    location: 'Zanzibar',
     responsibilities: [
       {
         id: nanoid(),
-        task: 'Web content monitoring and maintenance'
+        task: 'Development of the hotel\'s main website'
       },
       {
         id: nanoid(),
-        task: 'Computer hardware maintenance'
+        task: 'Creating and configuring the hotel\'s OVH accounts'
       },
       {
         id: nanoid(),
-        task: 'Network configuration'
-      },
-      {
-        id: nanoid(),
-        task: 'Training and sensitizing local fishermen on the growing digital market for marine products'
+        task: 'Deployment of the website'
       }
     ]
+
   },
   {
     id: nanoid(),
-    logo: cgk,
-    companyUrl: 'https://kwale.go.ke/',
-    companyName: 'Kwale County Government',
-    role: 'Vocational Training Officer - IT section',
-    companyDesc: 'Local authority division of vocational training focusing on imparting skills',
-    period: 'Jan, 2015 - Date',
-    location: 'Kwale, Kenya',
+    logo: edureka,
+    companyUrl:'https://www.edureka.co/',
+    companyName: 'Fullstack Internship Program - MERN Technologies',
+    role: 'FullStack developer',
+    companyDesc: 'A renowned Indian firm for training global IT experts',
+    period: 'March, 2024 - To Date',
+    location: 'Bangalore, India',
     responsibilities: [
       {
         id: nanoid(),
-        task: 'Collecting and collating youth training data',
+        task: 'Interpretation of coding tasks'
       },
       {
         id: nanoid(),
-        task: 'Training basic computing skills',
+        task: 'Development of frontEnd Applications - Javascript, ReactJs, Tailwind CSS'
       },
       {
         id: nanoid(),
-        task: 'Training youth on web development - HTML5, CSS3 and Javascript ',
+        task: 'Pair programming and Development of Fullstack Applications'
+      },
+      {
+        id: nanoid(),
+        task: 'Project presentation'
       }
+    ]
+    },
+    {
+      id: nanoid(),
+      logo: rakol,
+      companyUrl: 'https://raysoftwareservice.com/',
+      companyName: 'Rakolkata Software service private ltd',
+      role: 'FullStack Web Developer',
+      companyDesc: 'Software Development Company',
+      period: 'June, 2023 - Nov, 2023',
+      location: 'India',
+      responsibilities: [
+        {
+          id: nanoid(),
+          task: 'Analysis and Interpretation web based system requirements'
+        },
+        {
+          id: nanoid(),
+          task: 'Development of full stack applications - MERN Stack'
+        }
       ]
     },
     {
@@ -85,76 +106,54 @@ export const data = [
     },
     {
       id: nanoid(),
-      logo: rakol,
-      companyUrl: 'https://raysoftwareservice.com/',
-      companyName: 'Rakolkata Software service private ltd',
-      role: 'FullStack Web Developer',
-      companyDesc: 'Software Development Company',
-      period: 'June, 2023 - Nov, 2023',
-      location: 'India',
+      logo: cgk,
+      companyUrl: 'https://kwale.go.ke/',
+      companyName: 'Kwale County Government',
+      role: 'Vocational Training Officer - IT section',
+      companyDesc: 'Local authority division of vocational training focusing on imparting skills',
+      period: 'Jan, 2015 - Date',
+      location: 'Kwale, Kenya',
       responsibilities: [
         {
           id: nanoid(),
-          task: 'Analysis and Interpretation web based system requirements'
+          task: 'Collecting and collating youth training data',
         },
         {
           id: nanoid(),
-          task: 'Development of full stack applications - MERN Stack'
+          task: 'Training basic computing skills',
+        },
+        {
+          id: nanoid(),
+          task: 'Training youth on web development - HTML5, CSS3 and Javascript ',
         }
-      ]
-    },
-    {
+        ]
+      },
+      {
       id: nanoid(),
-      logo: pemba,
-      companyUrl:'https://pembaparadise.com/',
-      companyName: 'Pemba Beach Resort',
-      role: 'Web Developer',
-      companyDesc: 'Tour firm specializing in providing hotel services in Zanzibar',
-      period: 'Jan, 2024 - Mar, 2024',
-      location: 'Zanzibar',
+      logo: tecafili,
+      companyUrl: 'https://tecafili.org/',
+      companyName: 'TECAFILI',
+      role: 'IT Support Technician',
+      companyDesc: 'Technology for care and Livelihood - Non governmental Organization focusing on  training of fishers in mariculture',
+      period: 'April - Dec, 2014',
+      location: 'Mombasa, KE',
       responsibilities: [
         {
           id: nanoid(),
-          task: 'Modifying and improving the appearance hotel\'s main website'
+          task: 'Web content monitoring and maintenance'
         },
         {
           id: nanoid(),
-          task: 'Creating the hotel\'s OVH accounts'
+          task: 'Computer hardware maintenance'
         },
         {
           id: nanoid(),
-          task: 'Deployment of the website'
-        }
-      ]
-
-    },
-    {
-      id: nanoid(),
-      logo: edureka,
-      companyUrl:'https://www.edureka.co/',
-      companyName: 'Fullstack Internship Program - MERN Technologies',
-      role: 'FullStack developer',
-      companyDesc: 'A renowned Indian firm for training global IT experts',
-      period: 'March, 2024 - Date',
-      location: 'Bangalore, India',
-      responsibilities: [
-        {
-          id: nanoid(),
-          task: 'Interpretation of coding tasks'
+          task: 'Network configuration'
         },
         {
           id: nanoid(),
-          task: 'Development of frontEnd Applications - Javascript, ReactJs, Tailwind CSS'
-        },
-        {
-          id: nanoid(),
-          task: 'Pair programming and Development of Fullstack Applications'
-        },
-        {
-          id: nanoid(),
-          task: 'Project presentation'
+          task: 'Training and sensitizing local fishermen on the growing digital market for marine products'
         }
       ]
     }
-
 ]
