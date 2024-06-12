c0ac6024bc3ddc792499ed07aa9737f8dbd1e216
asnganje <abd.nganje@gmail.com>
2024-06-12 15:21:10 +0300

    Modify landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,7 @@ import profImg from '../assets/profileImg.jpeg'
 
 const Landing = () => {
     return <div className="land">
-                <h3 className='top-desc'>I want to <span className='things'>create things</span> <br/> that truly  
+                <h3 className='top-desc'>I want to <span className='things'>develop products</span> <br/> that will  
                 <span> </span><span className='diff'>make a difference</span></h3>
                 <img src={profImg} alt="profile-pic" className='profile-pic'/>
         </div>
