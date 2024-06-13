b13288cc2026a2d56ad7eeeb0deb86574c50334b
asnganje <abd.nganje@gmail.com>
2024-06-13 10:30:18 +0300

    Update resume url

--- src/data.jsx
+++ src/data.jsx
@@ -57,7 +57,7 @@ export const data = [
     },
     {
         id: nanoid(),
-        url: 'https://docs.google.com/document/d/1_oBjHhGMEi20EmzREYA7pAtsfZJoHO2TNic0XwNAF48/edit?usp=sharing',
+        url: 'https://docs.google.com/document/d/1Wmn74hUDx6uDCAkm0alOBRu87ugA3VQH/edit?usp=sharing&ouid=107744250363229063982&rtpof=true&sd=true',
         text:'Resume',
         icon: <MdOutlineStickyNote2 />,
         color:'#424242'
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,5 @@
 const Resume = () => {
-    return <a href="https://docs.google.com/document/d/1_oBjHhGMEi20EmzREYA7pAtsfZJoHO2TNic0XwNAF48/edit?usp=sharing" target="_blank">Resume</a>
+    return <a href="https://docs.google.com/document/d/1Wmn74hUDx6uDCAkm0alOBRu87ugA3VQH/edit?usp=sharing&ouid=107744250363229063982&rtpof=true&sd=true" target="_blank">Resume</a>
 }
 
 export default Resume;
