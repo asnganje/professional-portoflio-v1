d25f8d248727a7059445788c082875d3c6f664d4
asnganje <abd.nganje@gmail.com>
2025-03-23 10:53:17 +0300

    change link to word

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,5 @@
 const Resume = () => {
-    return <a href="https://drive.google.com/file/d/1LCqzP-5hcPmsHoC8bE_NBERFs16cIzVi/view?usp=sharing" target="_blank">Resume</a>
+    return <a href="https://docs.google.com/document/d/1L8mMmsSLdH2wt3eBWALevPinf-yQs9m7/edit?usp=sharing&ouid=107744250363229063982&rtpof=true&sd=true" target="_blank">Resume</a>
 }
 
 export default Resume;
